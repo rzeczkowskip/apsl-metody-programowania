932b3238a8388ba48abe88fe0fb1af957d021caf
Piotr Rzeczkowski <rzeczkowski.piotr@gmail.com>
2022-04-21 10:25:28 +0200

    Add 2022-04-04 homework

--- main.py
+++ main.py
@@ -5,4 +5,6 @@ if __name__ == '__main__':
     _helpers.menu('# Menu', [
         _helpers.MenuLabel("Zajęcia 2022-04-04"),
         _helpers.MenuItem("Algorytm Euklidesa – iteracyjnie oraz rekurencyjnie", euklides.run),
+        _helpers.MenuItem("Skracanie ułamków", skracanie_ulamkow.run),
+
     ])
